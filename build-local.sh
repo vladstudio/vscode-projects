4a9fbe85302be34d2ad5c04e2f00d6518b006d4e
Vlad Gerasimov <vlad@vlad.studio>
2026-02-17 17:41:03 +0400

    Update icons; streamline local build

--- build-local.sh
+++ build-local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run compile && npx @vscode/vsce package && code --install-extension workspaces-0.0.1.vsix && echo "Installed. Reload VS Code windows to activate."
+npm run compile && npx @vscode/vsce package --allow-missing-repository --skip-license && code --install-extension workspaces-0.0.1.vsix && echo "Installed. Reload VS Code windows to activate."
